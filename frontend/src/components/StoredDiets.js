5ea7ad201103602d449eb8b4e0917a1c92f3d11c
tomekk001 <tomekczyz001@wp.pl>
2024-12-24 01:34:26 +0100

    zmiana wizualna wyświetlania posiłków

--- frontend/src/components/StoredDiets.js
+++ frontend/src/components/StoredDiets.js
@@ -53,30 +53,36 @@ const StoredDiet = () => {
       <p>Węglowodany: {storedMacros.carbohydrates} g</p>
       <p>Tłuszcze: {storedMacros.fats} g</p>
       <h2>Wygenerowana dieta na miesiąc:</h2>
-      {storedDietPlan.map((dayPlan, index) => (
-        <div key={index}>
-          <h3>{dayPlan.day}</h3>
-          {dayPlan.meals.map((meal, i) => (
-            <div key={i} className="meal-item">
-              <p className="meal-type">{meals[i]}</p>
-              <p className="meal-name">{meal.nazwa}</p>
-              <p className="meal-details">
-                {Math.ceil(meal.kaloryczność)} kcal
-              </p>
-              <p className="meal-ingredients">{meal.krótkiPrzepis}</p>
-              <p className="meal-ingredients">Składniki: {meal.składniki}</p>
+      <div className="days-container">
+        {storedDietPlan.map((dayPlan, index) => (
+          <div key={index} className="day-card">
+            <h3>{dayPlan.day}</h3>
+            <div className="meals-container">
+              {dayPlan.meals.map((meal, i) => (
+                <div key={i} className="meal-card">
+                  <p className="meal-type">{meals[i]}</p>
+                  <p className="meal-name">{meal.nazwa}</p>
+                  <p className="meal-details">
+                    {Math.ceil(meal.kaloryczność)} kcal
+                  </p>
+                  <p className="meal-ingredients">{meal.krótkiPrzepis}</p>
+                  <p className="meal-ingredients">
+                    Składniki: {meal.składniki}
+                  </p>
+                </div>
+              ))}
             </div>
-          ))}
-          <p className="calories-macros">
-            Kalorie: {Math.ceil(dayPlan.dailyCalories)} kcal
-          </p>
-          <p className="calories-macros">
-            Białko: {Math.ceil(dayPlan.dailyProtein)} g, Węglowodany:{" "}
-            {Math.ceil(dayPlan.dailyCarbs)} g, Tłuszcze:{" "}
-            {Math.ceil(dayPlan.dailyFats)} g
-          </p>
-        </div>
-      ))}
+            <div className="calories-macros">
+              <p>Kalorie: {Math.ceil(dayPlan.dailyCalories)} kcal</p>
+              <p>
+                Białko: {Math.ceil(dayPlan.dailyProtein)} g, Węglowodany:{" "}
+                {Math.ceil(dayPlan.dailyCarbs)} g, Tłuszcze:{" "}
+                {Math.ceil(dayPlan.dailyFats)} g
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
